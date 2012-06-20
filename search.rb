0ac5bbe68da2e6f25477dd36b58fe91897cfa5f9
Alan Middleton <alanmiddleton@yahoo.com>
2012-06-19 20:15:08 -0600

    Added the search.rb file to the repository

---
+++ search.rb
@@ -0,0 +1,19 @@
+module SimpleTwitter
+  class Serach
+
+    TWITTER_SEARCH = "http://search.twitter.com/search.json"
+    attr_accessor (':per_page')
+    def  initialize
+      @per_page = 10
+    end
+
+    def search (query)
+      params = {
+        :q => query,
+        :rpp => @per_page,
+      }
+      response = HTTParty.get(TWITTER_SEARCH, :query => params)
+      JSON.parse(response.body)
+    end
+  end
+end
